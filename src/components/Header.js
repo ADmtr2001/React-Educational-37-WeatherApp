926079c8179425ecaaaeecd8ddeccd0831f9c52c
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-12-04 15:29:28 +0200

    change Header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
+import FormatListNumberedRtlIcon from "@mui/icons-material/FormatListNumberedRtl";
 
 const Header = () => {
-  return <Container>Header</Container>;
+  return (
+    <Container>
+      <FormatListNumberedIcon fontSize='large' />
+      <span>ToDo</span>
+      <FormatListNumberedRtlIcon fontSize='large' />
+    </Container>
+  );
 };
 
 export default Header;
@@ -19,4 +27,8 @@ const Container = styled.div`
   border-bottom-left-radius: 15px;
   border-bottom-right-radius: 15px;
   box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.2);
+
+  span {
+    margin: 0 10px;
+  }
 `;
